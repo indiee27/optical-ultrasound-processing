76959d2a3d15e73f92e19c72fdc4a8e34d18c219
indiee27 <rmapile@ucl.ac.uk>
2021-05-14 15:40:50 +0100

    analysis script started

--- Matlab Scripts/python_scripts/analse_2D_pullback.py
+++ Matlab Scripts/python_scripts/analse_2D_pullback.py
@@ -1,1 +1,21 @@
-from utils import *
+# imports
+from python_scripts/utils.py import all #import the functions from the utils script
+
+# name the directory
+directory = ("C:/Users/indie/Documents/GitHub/optical-ultrasound-processing/tungsten wire and finger imaging - debugging")
+
+# list the file prefixes
+file_list = file_listing_func(directory)
+
+# main loop
+for item_prefix in file_list:
+
+    # hydrophone parameters (dictionary) using the function from utils
+    hydrophone_parameters = params_import(item_prefix)
+
+    # import list of image files
+    images = data_import(directory, item_prefix)
+
+    # second loop
+    for file in images:
+        data = read_file(file)
